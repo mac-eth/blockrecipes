e4e3ca2b9e78cae01b4a7bc16498e89793a43b38
Mac Sweeny <macsweenydev@gmail.com>
2023-04-07 23:49:39 +1000

    Feat: CircleBackground for mobile images

---
+++ src/components/phone/CircleBackground.tsx
@@ -0,0 +1,50 @@
+import { CircleBackgroundProps } from "@/types";
+import { motion } from "framer-motion";
+import { useId } from "react";
+
+export function CircleBackground({
+  color,
+  width = 558,
+  height = 558,
+  ...props
+}: CircleBackgroundProps) {
+  let id = useId();
+
+  return (
+    <motion.svg
+      viewBox="0 0 558 558"
+      width={width}
+      height={height}
+      fill="none"
+      aria-hidden="true"
+      {...props}
+      initial={{ rotate: 0 }}
+      animate={{ rotate: 360 }}
+      transition={{ repeat: Infinity, ease: "linear", duration: 20 }}
+    >
+      <defs>
+        <linearGradient
+          id={id}
+          x1="79"
+          y1="16"
+          x2="105"
+          y2="237"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor={color} />
+          <stop offset="1" stopColor={color} stopOpacity="0" />
+        </linearGradient>
+      </defs>
+      <path
+        opacity=".2"
+        d="M1 279C1 125.465 125.465 1 279 1s278 124.465 278 278-124.465 278-278 278S1 432.535 1 279Z"
+        stroke={color}
+      />
+      <path
+        d="M1 279C1 125.465 125.465 1 279 1"
+        stroke={`url(#${id})`}
+        strokeLinecap="round"
+      />
+    </motion.svg>
+  );
+}
