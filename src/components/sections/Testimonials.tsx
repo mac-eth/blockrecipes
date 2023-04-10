ba643385ddc44458257a82a088b05b789b5b6a32
Mac Sweeny <macsweenydev@gmail.com>
2023-04-10 10:59:45 +1000

    Feat: Feature Icons

---
+++ src/components/sections/Testimonials.tsx
@@ -0,0 +1,72 @@
+import Binance from "~/assets/logos/binance.svg";
+import Container from "~/components/layout/Container";
+import Face1 from "~/assets/face1.avif";
+import Face2 from "~/assets/face2.avif";
+import Image from "next/image";
+import Spooky from "~/assets/logos/spooky.svg";
+
+const Testimonials = () => {
+  return (
+    <section className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
+            <Image className="h-12 self-start" src={Binance} alt="" />
+            <figure className="mt-10 flex flex-auto flex-col justify-between">
+              <blockquote className="text-lg leading-8 text-gray-900">
+                <p>
+                  “Amet amet eget scelerisque tellus sit neque faucibus non
+                  eleifend. Integer eu praesent at a. Ornare arcu gravida
+                  natoque erat et cursus tortor consequat at. Vulputate gravida
+                  sociis enim nullam ultricies habitant malesuada lorem ac.
+                  Tincidunt urna dui pellentesque sagittis.”
+                </p>
+              </blockquote>
+              <figcaption className="mt-10 flex items-center gap-x-6">
+                <Image
+                  className="h-14 w-14 rounded-full bg-gray-50"
+                  src={Face1}
+                  alt=""
+                />
+                <div className="text-base">
+                  <div className="font-semibold text-gray-900">
+                    Judith Black
+                  </div>
+                  <div className="mt-1 text-gray-500">CEO of Tuple</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+          <div className="flex flex-col border-t border-gray-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
+            <Image className="h-12 self-start" src={Spooky} alt="" />
+            <figure className="mt-10 flex flex-auto flex-col justify-between">
+              <blockquote className="text-lg leading-8 text-gray-900">
+                <p>
+                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
+                  proident duis dolore nulla veniam reprehenderit nisi officia
+                  voluptate incididunt exercitation exercitation elit. Nostrud
+                  veniam sint dolor nisi ullamco.”
+                </p>
+              </blockquote>
+              <figcaption className="mt-10 flex items-center gap-x-6">
+                <Image
+                  className="h-14 w-14 rounded-full bg-gray-50"
+                  src={Face2}
+                  alt=""
+                />
+                <div className="text-base">
+                  <div className="font-semibold text-gray-900">
+                    Joseph Rodriguez
+                  </div>
+                  <div className="mt-1 text-gray-500">CEO of Reform</div>
+                </div>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
