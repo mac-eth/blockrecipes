5157378afb515319880d5c77afba3c6ae1098952
Mac Sweeny <macsweenydev@gmail.com>
2023-04-10 20:16:57 +1000

    Feat: Testimonials

--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -1,71 +1,240 @@
-import Binance from "~/assets/logos/binance.svg";
-import Container from "~/components/layout/Container";
-import Face1 from "~/assets/face1.avif";
-import Face2 from "~/assets/face2.avif";
 import Image from "next/image";
-import Spooky from "~/assets/logos/spooky.svg";
+import Photo1 from "~/assets/testimonials/photo1.avif";
+import Photo2 from "~/assets/testimonials/photo2.avif";
+import React from "react";
+import SavvyCal from "~/assets/testimonials/savvycal.svg";
+
+const featuredTestimonial = {
+  body: "Integer id nunc sit semper purus. Bibendum at lacus ut arcu blandit montes vitae auctor libero. Hac condimentum dignissim nibh vulputate ut nunc. Amet nibh orci mi venenatis blandit vel et proin. Non hendrerit in vel ac diam.",
+  author: {
+    name: "Brenna Goyette",
+    handle: "brennagoyette",
+    imageUrl: Photo1,
+    logoUrl: SavvyCal,
+  },
+};
+
+const testimonials = [
+  [
+    [
+      {
+        body: "Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.",
+        author: {
+          name: "Leslie Alexander",
+          handle: "lesliealexander",
+          imageUrl: Photo2,
+        },
+      },
+      {
+        body: "Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.",
+        author: {
+          name: "Leslie Alexander",
+          handle: "lesliealexander",
+          imageUrl: Photo2,
+        },
+      },
+    ],
+    [
+      {
+        body: "Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.",
+        author: {
+          name: "Lindsay Walton",
+          handle: "lindsaywalton",
+          imageUrl: Photo2,
+        },
+      },
+      {
+        body: "Aut reprehenderit voluptatem eum asperiores beatae id. Iure molestiae ipsam ut officia rem nulla blanditiis.",
+        author: {
+          name: "Lindsay Walton",
+          handle: "lindsaywalton",
+          imageUrl: Photo2,
+        },
+      },
+    ],
+  ],
+  [
+    [
+      {
+        body: "Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.",
+        author: {
+          name: "Tom Cook",
+          handle: "tomcook",
+          imageUrl: Photo2,
+        },
+      },
+      {
+        body: "Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis recusandae saepe corrupti.",
+        author: {
+          name: "Tom Cook",
+          handle: "tomcook",
+          imageUrl: Photo2,
+        },
+      },
+    ],
+    [
+      {
+        body: "Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.",
+        author: {
+          name: "Leonard Krasner",
+          handle: "leonardkrasner",
+          imageUrl: Photo2,
+        },
+      },
+      {
+        body: "Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.",
+        author: {
+          name: "Leonard Krasner",
+          handle: "leonardkrasner",
+          imageUrl: Photo2,
+        },
+      },
+    ],
+  ],
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const Testimonials = () => {
   return (
-    <section className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
-            <Image className="h-12 self-start" src={Binance} alt="" />
-            <figure className="mt-10 flex flex-auto flex-col justify-between">
-              <blockquote className="text-lg leading-8 text-neutral-900">
-                <p>
-                  “Amet amet eget scelerisque tellus sit neque faucibus non
-                  eleifend. Integer eu praesent at a. Ornare arcu gravida
-                  natoque erat et cursus tortor consequat at. Vulputate gravida
-                  sociis enim nullam ultricies habitant malesuada lorem ac.
-                  Tincidunt urna dui pellentesque sagittis.”
-                </p>
-              </blockquote>
-              <figcaption className="mt-10 flex items-center gap-x-6">
-                <Image
-                  className="h-14 w-14 rounded-full bg-neutral-50"
-                  src={Face1}
-                  alt=""
-                />
-                <div className="text-base">
-                  <div className="font-semibold text-neutral-900">
-                    Judith Black
-                  </div>
-                  <div className="mt-1 text-neutral-500">CEO of Tuple</div>
-                </div>
-              </figcaption>
-            </figure>
+    <div className="relative isolate sm:py-32 bg-neutral-100">
+      <svg
+        className="absolute inset-0 -z-10 hidden h-full w-full stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,neutral-100,transparent)] sm:block"
+        aria-hidden="true"
+      >
+        <defs>
+          <pattern
+            id="55d3d46d-692e-45f2-becd-d8bdc9344f45"
+            width={200}
+            height={200}
+            x="50%"
+            y={0}
+            patternUnits="userSpaceOnUse"
+          >
+            <path d="M.5 200V.5H200" fill="none" />
+          </pattern>
+        </defs>
+        <svg x="50%" y={0} className="overflow-visible fill-gray-50">
+          <path
+            d="M-200.5 0h201v201h-201Z M599.5 0h201v201h-201Z M399.5 400h201v201h-201Z M-400.5 600h201v201h-201Z"
+            strokeWidth={0}
+          />
+        </svg>
+        <rect
+          width="100%"
+          height="100%"
+          strokeWidth={0}
+          fill="url(#55d3d46d-692e-45f2-becd-d8bdc9344f45)"
+        />
+      </svg>
+      <div className="relative">
+        <div
+          className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
+          aria-hidden="true"
+        >
+          <div
+            className="ml-[max(50%,38rem)] aspect-[1313/771] w-[82.0625rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div
+          className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-8 opacity-25 blur-3xl xl:justify-end"
+          aria-hidden="true"
+        >
+          <div
+            className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] xl:ml-0 xl:mr-[calc(50%-12rem)]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-xl sm:text-center">
+            <h2 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">
+              Testimonials
+            </h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              We have worked with thousands of amazing people
+            </p>
           </div>
-          <div className="flex flex-col border-t border-neutral-900/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
-            <Image className="h-12 self-start" src={Spooky} alt="" />
-            <figure className="mt-10 flex flex-auto flex-col justify-between">
-              <blockquote className="text-lg leading-8 text-neutral-900">
-                <p>
-                  “Excepteur veniam labore ullamco eiusmod. Pariatur consequat
-                  proident duis dolore nulla veniam reprehenderit nisi officia
-                  voluptate incididunt exercitation exercitation elit. Nostrud
-                  veniam sint dolor nisi ullamco.”
-                </p>
+          <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 grid-rows-1 gap-8 text-sm leading-6 text-gray-900 sm:mt-20 sm:grid-cols-2 xl:mx-0 xl:max-w-none xl:grid-flow-col xl:grid-cols-4">
+            <figure className="col-span-2 hidden sm:block sm:rounded-2xl sm:bg-neutral-100 sm:shadow-lg sm:ring-1 sm:ring-gray-900/5 xl:col-start-2 xl:row-end-1">
+              <blockquote className="p-12 text-xl font-semibold leading-8 tracking-tight text-gray-900">
+                <p>{`“${featuredTestimonial.body}”`}</p>
               </blockquote>
-              <figcaption className="mt-10 flex items-center gap-x-6">
+              <figcaption className="flex items-center gap-x-4 border-t border-gray-900/10 px-6 py-4">
                 <Image
-                  className="h-14 w-14 rounded-full bg-neutral-50"
-                  src={Face2}
+                  className="h-10 w-10 flex-none rounded-full bg-gray-50"
+                  src={featuredTestimonial.author.imageUrl}
                   alt=""
                 />
-                <div className="text-base">
-                  <div className="font-semibold text-neutral-900">
-                    Joseph Rodriguez
+                <div className="flex-auto">
+                  <div className="font-semibold">
+                    {featuredTestimonial.author.name}
                   </div>
-                  <div className="mt-1 text-neutral-500">CEO of Reform</div>
+                  <div className="text-gray-600">{`@${featuredTestimonial.author.handle}`}</div>
                 </div>
+                <Image
+                  className="h-10 w-auto flex-none"
+                  src={featuredTestimonial.author.logoUrl}
+                  alt=""
+                />
               </figcaption>
             </figure>
+            {testimonials.map((columnGroup, columnGroupIdx) => (
+              <div
+                key={columnGroupIdx}
+                className="space-y-8 xl:contents xl:space-y-0"
+              >
+                {columnGroup.map((column, columnIdx) => (
+                  <div
+                    key={columnIdx}
+                    className={classNames(
+                      (columnGroupIdx === 0 && columnIdx === 0) ||
+                        (columnGroupIdx === testimonials.length - 1 &&
+                          columnIdx === columnGroup.length - 1)
+                        ? "xl:row-span-2"
+                        : "xl:row-start-1",
+                      "space-y-8"
+                    )}
+                  >
+                    {column.map((testimonial) => (
+                      <figure
+                        key={testimonial.author.handle}
+                        className="rounded-2xl bg-neutral-100 p-6 shadow-lg ring-1 ring-gray-900/5"
+                      >
+                        <blockquote className="text-gray-900">
+                          <p>{`“${testimonial.body}”`}</p>
+                        </blockquote>
+                        <figcaption className="mt-6 flex items-center gap-x-4">
+                          <Image
+                            className="h-10 w-10 rounded-full bg-gray-50"
+                            src={testimonial.author.imageUrl}
+                            alt=""
+                          />
+                          <div>
+                            <div className="font-semibold">
+                              {testimonial.author.name}
+                            </div>
+                            <div className="text-gray-600">{`@${testimonial.author.handle}`}</div>
+                          </div>
+                        </figcaption>
+                      </figure>
+                    ))}
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
